9e75549a7756c00cd3a95791f07890f829f2b9cb
tumaolin94 <524561223@qq.com>
2015-08-08 13:07:05 +0800

    Vectorize
    Vectorize the code

--- machine-learning-ex1/gradientDescent.m
+++ machine-learning-ex1/gradientDescent.m
@@ -16,9 +16,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-    temp0 = theta(1) - alpha*sum(X*theta-y)/m;
-    temp1 = theta(2) - alpha*sum((X*theta-y).*X(:,2))/m;
-    theta = [temp0;temp1];
+
+
+theta=theta-alpha*X'*(X*theta-y)/m;
 
     % ============================================================
 
